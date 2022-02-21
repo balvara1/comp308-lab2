117c0e5ce13f474563633186b16f915e85775369
balvara1 <84111063+balvara1@users.noreply.github.com>
2022-02-21 13:56:50 -0500

    added get functionalities for students

--- app/routes/students.server.routes.js
+++ app/routes/students.server.routes.js
@@ -1,12 +1,32 @@
-//This uses CommonJS module pattern to export a single module function.
-//This function takes an express object as argument 
-
-//Load the 'index' controller
-var student = require('../controllers/students.server.controller');
-//
-//handle routing for get and post request
+//Load the 'students' controller
+var students = require('../controllers/students.server.controller');
+var express = require('express');
+var router = express.Router();
+// Define the routes module' method
 module.exports = function (app) {
-    //handle a get request made to root path
-    app.get('/student', student.render); //go to http://localhost:3000/student
-
+    // handle a get request made to /students path
+    // and list students when /students link is selected
+    //app.get("/students",students.requiresLogin,students.list); // go to http://localhost:3000/students
+    app.get("/students", students.list);
+    app.post('/', students.create);
+    
+    //
+    // Set up the 'students' parameterized routes
+    app.route('/students/:studentId')
+    .get(students.read)
+    //.put(students.update)
+    //.delete(students.delete)
+    // Set up the 'studentId parameter middleware
+    // All param callbacks will be called before any handler of
+    // any route in which the param occurs, and they will each
+    // be called only once in a request - response cycle,
+    // even if the parameter is matched in multiple routes
+    app.param('studentId', students.studentByID);
+    // authenticate student
+    //app.post('/signin', students.authenticate);
+    //app.get('/signout', student.signout);
+    //app.get('/read_cookie', students.isSignedIn);
+    
+    // path to a protected page
+    //app.get('/welcome',students.welcome);
 };
